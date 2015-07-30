a11d2721f2d23c9d9e3db95dfaf430693d0fb565
DavidGhedini <david@davidghedini.com>
2015-07-30 19:43:21 +0200

    MapFig Layer Model
    MapFig Layer Model

---
+++ models/MapfigLayer.php
@@ -0,0 +1,11 @@
+<?php
+
+class MapfigLayer extends Omeka_Record_AbstractRecord
+{
+    public $id;
+    public $name;
+    public $url;
+    public $key;
+    public $accesstoken;
+    public $attribution;
+}
